bf23a4bfc801f0f92c3cc3517199b73b3ab5d54d
jonesqpacman <adiardiansyah.official@gmail.com>
2021-09-25 23:35:32 +0700

    franklin : initial build releases

---
+++ BoardConfig.mk
@@ -0,0 +1,137 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/amlogic/franklin
+
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+TARGET_USES_64_BIT_BINDER := true
+ENABLE_CPUSETS := true
+
+# Platform
+BOARD_VENDOR := amlogic
+TARGET_SOC := s905x2
+TARGET_BOARD_HARDWARE := amlogic
+TARGET_BOARD_PLATFORM := g12a
+TARGET_BOARD_PLATFORM_GPU := mali-g31mp2
+TARGET_BOOTLOADER_BOARD_NAME := u212
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Graphics
+BOARD_EGL_CFG := $(DEVICE_PATH)/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := androidboot.dynamic_partitions=true androidboot.dtbo_idx=0 androidboot.boot_devices=ffe07000.emmc buildvariant=user androidboot.selinux=permissive
+
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_BASE := 0x01078000 
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0xfef88100
+BOARD_RAMDISK_OFFSET     := 0xfff88000
+BOARD_DTB_OFFSET := 0x00e88000
+BOARD_KERNEL_SECOND_OFFSET := 0xffe88000
+BOARD_BOOTIMG_HEADER_VERSION := 2
+
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --second $(DEVICE_PATH)/prebuilt/second 
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824
+BOARD_SUPER_PARTITION_SIZE := 1677721600
+BOARD_SUPER_PARTITION_GROUPS := amlogic_dynamic_partitions
+BOARD_AMLOGIC_DYNAMIC_PARTITIONS_SIZE := 1677721600
+BOARD_AMLOGIC_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm
+
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Recovery
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# File systems
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Metadata
+BOARD_USES_METADATA_PARTITION := true
+BOARD_ROOT_EXTRA_FOLDERS += metadata
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Android Verified Boot
+BOARD_AVB_ENABLE := false
+
+# Debug
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+TWRP_EVENT_LOGGING := false
+
+# Crypto
+PLATFORM_SECURITY_PATCH := 2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+
+# TWRP specific build flags
+RECOVERY_VARIANT := twrp
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+TW_THEME := landscape_hdpi
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_INCLUDE_NTFS_3G := true
+TW_EXTRA_LANGUAGES := true
+TW_USE_NEW_MINADBD := true
+TW_USE_TOOLBOX := true
+TW_NO_BATT_PERCENT := true
+TW_NO_SCREEN_TIMEOUT := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_SKIP_COMPATIBILITY_CHECK := true
+TARGET_USES_MKE2FS := true
+TW_NO_LEGACY_PROPS := true
+TW_DEFAULT_LANGUAGE := id_ID
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
+TARGET_USES_MKE2FS := true
+TW_DEVICE_VERSION := $(shell date '+%Y%m%d') by JonesqPacMan™
+
+# Platform version
+PLATFORM_VERSION := 16.1.0
+ 
